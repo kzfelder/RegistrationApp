fda3c2e79455bdd7545c90dcfcb24c32d73f742b
kzfelder <35270718+kzfelder@users.noreply.github.com>
2018-03-22 14:42:08 -0500

    read and store data from file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         System.out.println("\n\nHello, RegistrationApp!\n");
+
+        System.out.println(readFromFile("curriculum.dat"));
+
+
+        //Read and stMethod to ore the data from "curriculum.dat"
+        // write the data to stdout
+        // Calculate the total hours in the curriculum
+        // Count the number of [DEPT] courses
+        // Determine if a course is in the curriculum.
     }
+}
+
+// read and store data from file
+private static ArrayList<String> readFromFile(String fileName)
+{
+    ArrayList<String> data = new ArrayList<>();
+    File file = new File(fileName);
+    try (Scanner sc = new Scanner(file))
+    {
+        while (sc.hasNext())
+        {
+            data.add(sc.nextLine());
+        }
+    }
+    catch (FileNotFoundException e)
+    {
+        e.printStackTrace();
+    }
+    return data;
 }
